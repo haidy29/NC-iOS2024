4b807cde5f98f10dc3336c880216f908a1501aab
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-08-02 13:56:55 +0300

    adding comments

--- calculator.swift
+++ calculator.swift
@@ -2,9 +2,11 @@ struct Calculator {
     var n1 : Int
     var n2 : Int
     
+    //Ahmed's part
     func add(n1:Int , n2:Int)->Int {
         
     }
+    //haidy's part 
     func sub (n1:Int , n2:Int)->Int {
         
     }
